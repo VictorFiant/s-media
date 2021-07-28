ac1050bb982dc8a644c1e51383f0555003eadeca
VictorFiant <77025466+VictorFiant@users.noreply.github.com>
2021-07-28 21:33:15 +0100

    new

---
+++ src/entities/Comment.ts
@@ -0,0 +1,50 @@
+import { Entity as TOEntity, Column, ManyToOne, BeforeInsert, JoinColumn, Index, OneToMany } from "typeorm";
+import { Exclude } from "class-transformer";
+import { makeId } from "../util/helper"
+import Post from './Post'
+import User from './User'
+import Entity from './Entity'
+import Vote from './Vote';
+
+
+@TOEntity('comments')
+export default class Comment extends Entity {
+    constructor(comment: Partial<Comment>) {
+        super()
+        Object.assign(this, comment)
+    }
+
+    @Index()
+    @Column()
+    identifier: string // 7 character Id
+
+    @Column()
+    body: string
+
+    @Column()
+    username: string
+
+    @ManyToOne(() => User)
+    @JoinColumn({ name: 'username', referencedColumnName: 'username' })
+    user: User
+
+    @ManyToOne(() => Post, (post) => post.comments, { nullable: false })
+    post: Post
+
+    @Exclude()
+    @OneToMany(() => Vote, (vote) => vote.comment)
+    votes: Vote[]
+
+    protected userVote: number
+    setUserVote(user: User) {
+        const index = this.votes?.findIndex((v) => v.username === user.username)
+        this.userVote = index > -1 ? this.votes[index].value : 0
+    }
+
+    @BeforeInsert()
+    makeIdAndSlug() {
+        this.identifier = makeId(8)
+
+    }
+
+}
---
+++ src/routes/subs.ts
@@ -0,0 +1,112 @@
+import { Request, Response, Router } from 'express';
+import { isEmpty } from 'class-validator';
+import { getRepository } from 'typeorm';
+import { makeId } from '../util/helper';
+import auth from '../middleware/auth';
+import user from '../middleware/user';
+import Sub from '../entities/Sub';
+import User from '../entities/User';
+import Post from '../entities/Post';
+import multer, { FileFilterCallback } from 'multer';
+import path from 'path';
+
+
+
+const createSub = async (req: Request, res: Response) => {
+    const { name, title, description } = req.body
+
+    const user: User = res.locals.user
+
+    try {
+        let errors: any = {}
+        if (isEmpty(name)) errors.name = 'Name must not be empty'
+        if (isEmpty(title)) errors.title = 'Title must not be empty'
+
+        const sub = await getRepository(Sub)
+            .createQueryBuilder('sub')
+            .where('lower(sub.name) = :name', { name: name.toLowerCase() })
+            .getOne()
+
+        if (sub) errors.name = 'Sub exists already'
+
+        if (Object.keys(errors).length > 0) {
+            throw errors
+        };
+    } catch (err) {
+        return res.status(400).json(err)
+    };
+
+    try {
+        const sub = new Sub({ name, description, title, user })
+        await sub.save()
+
+        return res.json(sub)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({ error: 'Something has error' })
+    };
+};
+/**
+ * 
+ * @param req 
+ * @param res 
+ * @returns 
+ */
+
+
+const getSub = async (req: Request, res: Response) => {
+    const name = req.params.name
+
+    try {
+        const sub = await Sub.findOneOrFail({ name })
+        const posts = await Post.find({
+            where: { sub },
+            order: { createdAt: 'DESC' },
+            relations: ['comments', 'votes']
+        })
+        sub.posts = posts
+
+        if (res.locals.user) {
+            sub.posts.forEach((p) => p.setUserVote(res.locals.user))
+        }
+
+        return res.json(sub)
+    } catch (err) {
+        console.log(err)
+        return res.status(404).json({ sub: 'Sub not found' })
+    }
+}
+
+/**
+ * 
+ */
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: 'public/images',
+        filename: (req, file, callback) => {
+            const name = makeId(15)
+            callback(null, name + path.extname(file.originalname))
+        }
+    }),
+    fileFilter: (_, file: any, callback: FileFilterCallback) => {
+        if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+            callback(null, true)
+        } else {
+            callback(null, true)
+        }
+    }
+})
+
+const uploadSubImage = async (req: Request, res: Response) => {
+return res.json({success: true})
+}
+
+
+const router = Router()
+
+router.post('/', user, auth, createSub)
+router.get('/:name', user, getSub)
+router.post('/:name/image', user, auth, upload.single('file'), uploadSubImage)
+
+export default router;
